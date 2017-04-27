ac713c210f1a0860fce9382f8618a81a98250409
mrmedfac9 <ramefa@hotmail.com>
2017-04-27 18:21:21 +0200

    Botón de ayuda

--- app/src/main/java/rafalex/pdm/ugr/vrfurniture/MainActivity.java
+++ app/src/main/java/rafalex/pdm/ugr/vrfurniture/MainActivity.java
@@ -5,9 +5,37 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Datos para dialogos
+    private AlertDialog menuDialog;
+    private AlertDialog.Builder helpDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Crea los dialogos
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.help_dialog, null);
+
+        helpDialog = new AlertDialog.Builder(this, R.style.DialogTheme)
+                .setView(dialogView)
+                .setTitle(R.string.menu_help)
+                .setNeutralButton(R.string.ok_button,null);
+
+        menuDialog = helpDialog.create();
+    }
+
+    //Función onOptionItemSelected, para definir el funcionamiento de las opciones del action_bar
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        if (item.getItemId() == R.id.help) {
+
+            menuDialog.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
